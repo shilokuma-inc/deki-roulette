5a45d6df0928506116e83ada97264ab57d637763
Takumi Muraishi <mrs1669.develop@gmail.com>
2026-09-21 05:59:44 +0900

    [feat] 画面収録・ミラーリング中は印を伏せる

--- DekiRoulette/App/DekiRouletteApp.swift
+++ DekiRoulette/App/DekiRouletteApp.swift
@@ -2,10 +2,14 @@ import SwiftUI
 
 @main
 struct DekiRouletteApp: App {
+    /// 画面収録・ミラーリングの状態。両画面の `ItemListView` が印を伏せる判断に使う。
+    @State private var screenCapture = ScreenCaptureMonitor(source: UIScreenCaptureSource())
+
     var body: some Scene {
         WindowGroup {
             RootView()
                 .fontDesign(.rounded)
+                .environment(screenCapture)
         }
     }
 }
